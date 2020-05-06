6692c70ad4c0b94bfe59d85a62d258de5df7a757
Jun Xia <jx967@hotmail.com>
2020-05-06 22:28:52 +1000

    Updates

---
+++ precache-manifest.2a3ff1e3cdee7cf45a050490a3488781.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca4ac94bf031860c231324c81fa3820e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6d053a0c2bf81e88b807",
+    "url": "/monsters-rolodex/static/css/main.e712f12f.chunk.css"
+  },
+  {
+    "revision": "f7b84cd810271619eca9",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d053a0c2bf81e88b807",
+    "url": "/monsters-rolodex/static/js/main.7f222a5a.chunk.js"
+  },
+  {
+    "revision": "e3f9453037e5f28f8167",
+    "url": "/monsters-rolodex/static/js/runtime-main.7c0f1f0f.js"
+  }
+]);
